f323dab0b8e5f85a4d78882f08a88806e6cd5853
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-02 13:35:52 -0500

    Agregado de foto

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,8 +1,13 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +16,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
@@ -22,6 +30,17 @@ public class ClienteController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	@GetMapping("/ver/{id}")
+	public String ver(@PathVariable (value="id")Long id,Map<String,Object> model) {
+		Cliente cliente =clienteService.findOne(id);
+		if(cliente.getId() == null) {
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo","Detalle cliente "+ cliente.getNombre());
+		return "ver";
+	}
 
 	@GetMapping(value = "/listar")
 	public String listar(Model model) {
@@ -40,12 +59,28 @@ public class ClienteController {
 	}
 	
 	@PostMapping(value="/form" )
-	public String guardar(@Valid Cliente cliente, BindingResult result,Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result,Model model,@RequestParam ("file") MultipartFile foto,SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo","Formulario del cliente");
 			return "form";
 		}
+		if(!foto.isEmpty()) {
+			String rootPath = "/Users/rodrigolopezmendez/Documents/upload";
+			
+			try {
+				byte[] bytes = foto.getBytes();
+				Path rutaCompleta = Paths.get(rootPath+"/"+foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				
+				cliente.setFoto(foto.getOriginalFilename());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
 		clienteService.save(cliente);
+		status.setComplete();
 		return "redirect:listar";
 	}
 	
--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable {
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
+	
+	private String foto;
 
 	public Long getId() {
 		return id;
@@ -76,6 +78,14 @@ public class Cliente implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 	/**

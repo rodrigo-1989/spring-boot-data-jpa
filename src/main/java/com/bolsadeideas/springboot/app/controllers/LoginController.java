0b6423ad6029122ea2920d0d736d9d03eb395263
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-03 23:38:10 -0500

    abrir y cerrar session

--- src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -5,16 +5,20 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 	@GetMapping(value = "/login")
-	public String login(Model model, Principal principal) {
+	public String login(@RequestParam (value = "error" ,required = false) String error,Model model, Principal principal) {
 		
 		if(principal != null) {
 			System.out.println("*****Ya has iniciado session*****");
 			return "redirect:/";
+		}
+		if(error != null) {
+			model.addAttribute("error","Error en en las credenciales");
 		}
 		return "login";
 	}
